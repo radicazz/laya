b24ee3b62d54e1eb4ee7102a07a58d819db525cb
cazz <radicazz.dev@gmail.com>
2025-12-10 15:52:13 -0500

    feat(surfaces): add pixel format types

--- include/laya/laya.hpp
+++ include/laya/laya.hpp
@@ -7,6 +7,7 @@
 #include "events/event_types.hpp"
 #include "events/event_polling.hpp"
 #include "renderers/renderer.hpp"
+#include "surfaces/pixel_format.hpp"
 #include "windows/window.hpp"
 #include "subsystems.hpp"
 #include "errors.hpp"
